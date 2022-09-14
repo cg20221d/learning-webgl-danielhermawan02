b00a856bcce111982f64012b950517a7761e2bda
Daniel Hermawan <daniel.hermawan02@gmail.com>
2022-09-14 15:25:38 +0700

    done

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ function main() {
     var kanvas = document.getElementById("kanvas");
     var gl = kanvas.getContext("webgl");
 
+    var vertices = [
+        0.5, 0.5, 
+        0.0, 0.0, 
+        -0.5, 0.5,
+        0.0, 1.0  
+    ];
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
     // Vertex Shader
     var vertexShaderCode = `
+    attribute vec2 aPosition;
     void main() {
-        float x = 0.0;
-        float y = 0.0;
+        float x = aPosition.x;
+        float y = aPosition.y;
         gl_PointSize = 10.0;
         gl_Position = vec4(x, y, 0.0, 1.0);
     }
@@ -37,10 +49,17 @@ function main() {
     gl.linkProgram(shaderProgram);
 
     gl.useProgram(shaderProgram); // Running Program...
-3
+
+    // Mengajari GPU bagaimana carai mengoleksi
+    // nilai posisi dari ARRAY_BUFFER
+    // untuk setiap vertex yang sedang diproses
+
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(1.0, 0.65, 0.0, 1.0); // Red , Green , Blue , Alpha
     gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.drawArrays(gl.POINTS, 0, 8);
+    gl.drawArrays(gl.TRIANGLES_FAN, 0, 3); // Berapa banyak iterasi yang diperlukan untuk menggambar
 
 }
