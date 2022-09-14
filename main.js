dac4755d037a0395632087249f8aac102c473666
Daniel Hermawan <daniel.hermawan02@gmail.com>
2022-09-14 14:51:49 +0700

    update pertemuan ke-2

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ function main() {
     var gl = kanvas.getContext("webgl");
 
     // Vertex Shader
-    var vertexShaderCode = 
-    "void main () {" +
-    "}";
+    var vertexShaderCode = `
+    void main() {
+        float x = 0.0;
+        float y = 0.0;
+        gl_PointSize = 10.0;
+        gl_Position = vec4(x, y, 0.0, 1.0);
+    }
+    `;
 
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShaderObject , vertexShaderCode);
@@ -13,8 +18,12 @@ function main() {
 
     // Fragment Shader
     var fragmentShaderCode = `
+    precision mediump float;
     void main() {
-
+        float r = 0.0;
+        float g = 0.0;
+        float b = 1.0;
+        gl_FragColor = vec4(r, g, b, 1.0);
     }
     `;
 
@@ -28,8 +37,10 @@ function main() {
     gl.linkProgram(shaderProgram);
 
     gl.useProgram(shaderProgram); // Running Program...
-
-    gl.clearColor(0.0, 0.0, 0.0, 0.5); // Red , Green , Blue , Alpha
+3
+    gl.clearColor(1.0, 0.65, 0.0, 1.0); // Red , Green , Blue , Alpha
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 8);
 
 }
